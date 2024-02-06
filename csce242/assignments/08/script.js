2d531b019ae32ddaa6aebea9d5f924b13814137d
cowmama7 <emerson.mitchell05@gmail.com>
2024-02-06 13:15:36 -0500

    polished assignment 08

--- csce242/assignments/08/script.js
+++ csce242/assignments/08/script.js
@@ -5,27 +5,27 @@ window.onload = () => {
 
     const SwapImg = () => {
         console.log(textVal);
-        if(textVal=="b") {
-            image.src="./images/read.jpg"
-        } else if(textVal=="c") {
-            image.src="./images/clown.jpg";
-        } else if(textVal=="p") {
-            image.src="./images/birthday.jpg";
-        } else if(textVal=="r") {
-            image.src="./images/rain.jpg";
-        } else if(textVal=="s") {
-            image.src="./images/shovel.jpg";
-        } else if(textVal=="w") {
-            image.src="./images/work.jpg";
+        if (textVal == "b") {
+            image.src = "./images/read.jpg"
+        } else if (textVal == "c") {
+            image.src = "./images/clown.jpg";
+        } else if (textVal == "p") {
+            image.src = "./images/birthday.jpg";
+        } else if (textVal == "r") {
+            image.src = "./images/rain.jpg";
+        } else if (textVal == "s") {
+            image.src = "./images/shovel.jpg";
+        } else if (textVal == "w") {
+            image.src = "./images/work.jpg";
         } else {
-            image.src="./images/original.jpg";
+            image.src = "./images/original.jpg";
         }
 
     }
 
     textInput.oninput = () => {
-      textVal = textInput.value.slice(-1);
-      SwapImg();
+        textVal = textInput.value.slice(-1);
+        SwapImg();
     };
 
     const slider = document.getElementById("slider1");
@@ -37,24 +37,23 @@ window.onload = () => {
     slider.oninput = () => {
         slideVal = slider.value;
         rot();
-      };
+    };
 
 
-
-      const ex1 = document.getElementById("ex1");
-      const ex2 = document.getElementById("ex2");
-      let box1 = document.getElementById("box1");
-      let box2 = document.getElementById("box2");
-      box1.style.display = "block";
-      box2.style.display = "none";
-      ex1.onclick = () => {
+    const ex1 = document.getElementById("ex1");
+    const ex2 = document.getElementById("ex2");
+    let box1 = document.getElementById("box1");
+    let box2 = document.getElementById("box2");
+    box1.style.display = "block";
+    box2.style.display = "none";
+    ex1.onclick = () => {
         box2.style.display = 'none';
         box1.style.display = 'block';
-      }
-      ex2.onclick = () => {
+    }
+    ex2.onclick = () => {
         box1.style.display = 'none';
         box2.style.display = 'block';
-      }
+    }
 
-      
+
 };
