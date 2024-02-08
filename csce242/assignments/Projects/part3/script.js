de665ae1e85399bf7664db4d38ae7268f8f718d2
cowmama7 <emerson.mitchell05@gmail.com>
2024-02-08 14:15:30 -0500

    implemented hamburger menu for all pages

--- csce242/assignments/Projects/part3/script.js
+++ csce242/assignments/Projects/part3/script.js
@@ -1,15 +1,8 @@
 function myFunction() {
-    let x = document.getElementById("myLinks");
-    if (x.style.display === "block") {
-      x.style.display = "none";
-    } else {
-      x.style.display = "block";
-    }
-
-    let y = document.getElementById("learnMoreButton");
-    let z = document.getElementById("alert");
-    y.onclick = () => {
-      z.style.display="block";
-    }
-    
+  let x = document.getElementById("myLinks");
+  if (x.style.display === "block") {
+    x.style.display = "none";
+  } else {
+    x.style.display = "block";
   }
+}
