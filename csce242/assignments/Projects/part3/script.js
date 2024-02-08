0bb542b4d67a06d022c99f5cd527e106ba8adce6
cowmama7 <emerson.mitchell05@gmail.com>
2024-02-08 09:24:12 -0500

    finished up the thing

--- csce242/assignments/Projects/part3/script.js
+++ csce242/assignments/Projects/part3/script.js
@@ -1,8 +1,15 @@
 function myFunction() {
-    var x = document.getElementById("myLinks");
+    let x = document.getElementById("myLinks");
     if (x.style.display === "block") {
       x.style.display = "none";
     } else {
       x.style.display = "block";
     }
+
+    let y = document.getElementById("learnMoreButton");
+    let z = document.getElementById("alert");
+    y.onclick = () => {
+      z.style.display="block";
+    }
+    
   }
