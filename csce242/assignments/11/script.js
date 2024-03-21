cd77a2e79c91d64ee8447fb650584a55d558662f
cowmama7 <emerson.mitchell05@gmail.com>
2024-03-21 10:06:43 -0400

    updated main page to include assignment 14

--- csce242/assignments/11/script.js
+++ csce242/assignments/11/script.js
@@ -9,7 +9,7 @@ window.onload = () => {
             this.notes = notes;
             this.imgId = imgId;
         }
-    }
+    } 
 
     let arr = [new train("charger", "Charger", "Siemens", "125 Mph", "Amtrak Long Range routes, and select commuter routes", "The Charger is one of the newest trains to enter service on Amtrak's lines. Manufactured by Siemens and painted in the new phase VI livery, these trains mark the beginning of the phase VI overhaul of Amtrak's rolling stock. These trains have an upgraded 125 Mph top speed, with goals to bring higher-speed rail service to existing Amtrak routes.", "charger3.jpg"),
     new train("acelaExpress", "Acela Express", "Alstom", "150 Mph", "Amtrac NEC from DC to Boston", "The Acela Express is the first high-speed rail line in the United States, entering service in 2000, connecting the DC, Baltimore, Philadelphia, New York, New Haven, and Boston, as well as select cities in between. With an operating top speed of 150 Mph, regular service every day, and unrivaled comfort, this train has become a very attractive option for business travelers on the East Coast.", "acelaExpress.jpg"),
@@ -17,6 +17,7 @@ window.onload = () => {
     new train("aveliaLiberty", "Avelia Liberty", "Alstom", "160 Mph (service)", "Amtrac NEC from DC to Boston", "The Avelia Liberty trainsets are set to replace the current Acela Express trains across the Northeast Corridor. Unfortuantely, they have faced countless delays due to recalls, and issues in their system, and currently sit in a yard in Philadelphia. These trains are expected to enter service in 2024.", "avelia-liberty.png")
     ];
     const imgClick = document.getElementsByClassName("assignment");
+    console.log(imgClick);
 
     for (const element of imgClick) {
         element.addEventListener("click", e => {
